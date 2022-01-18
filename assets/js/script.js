1171c87aa28a22043df4f110919aebe24928f9fd
Sara Gochez <saragochez13@gmail.com>
2022-01-17 19:53:27 -0800

    added styling to page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+function startQuiz() {}
+
+function nextQuestion() {}
+
+function selectCorrectAnswer() {}
+
+// should I make a function for wrong answers? or should those be in if/else statements?
